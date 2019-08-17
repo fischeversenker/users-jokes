9f97092629c18a23d6a05572be0ea8ceac42141c
fischeversenker <fischeversenker@users.noreply.github.com>
2019-08-17 23:07:30 +0200

    Update build.js

--- build.js
+++ build.js
@@ -1,4 +1,10 @@
 const fs = require('fs');
 
 const publicFiles = fs.readdirSync('public');
+
+// make sure dist directory exists
+if (!fs.existsSync('dist')) {
+  fs.mkdirSync('dist');
+}
+
 publicFiles.forEach(file => fs.copyFileSync(`public/${file}`, `dist/${file}`));
